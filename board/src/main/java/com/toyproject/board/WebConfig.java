c58da1db6aa2285615b30d475cefb997b2ab5408
한성 <hans.kk0725@gmail.com>
2025-03-22 22:22:59 +0900

    update board

---
+++ board/src/main/java/com/toyproject/board/WebConfig.java
@@ -0,0 +1,19 @@
+package com.toyproject.board;
+
+import com.toyproject.board.domain.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "/logout", "/css/**", "/error");
+
+    }
+}
---
+++ board/src/main/java/com/toyproject/board/domain/interceptor/SessionConst.java
@@ -0,0 +1,5 @@
+package com.toyproject.board.domain.interceptor;
+
+public interface SessionConst {
+    String LOGIN_MEMBER = "loginMember";
+}
